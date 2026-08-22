66057fbda9d33eeb496d4de00308100f8b7fea56
Spisila <gabrielspisila1@gmail.com>
2026-08-22 13:26:17 -0300

    refactor: Makes console logging Episodes into separate class

--- main.py
+++ main.py
@@ -53,10 +53,6 @@ class DQN(nn.Module):
             DQN.reset_target_counter()
 
         
-        
-
-    
-    
 
 class ReplayBuffer:
     def __init__(self, capacity=50000):
@@ -70,6 +66,47 @@ class ReplayBuffer:
     
     def size(self) :
         return len(self.buffer)
+
+class EpisodeLogger:
+
+    log_counter = 0
+
+    episode_count = 0
+    pieces_placed_sum = 0
+    lines_cleared_sum = 0
+
+    def __init__(self, log_counter_max):
+        self.log_counter_max = log_counter_max
+
+    @classmethod
+    def increase_log_counter(cls) :
+        cls.log_counter += 1
+        cls.episode_count += 1
+
+    @classmethod
+    def reset_log_counter(cls) :
+        cls.log_counter = 0
+
+    @classmethod
+    def console_log_episode(cls, pieces_placed, lines_cleared) :
+
+        cls.pieces_placed_sum += pieces_placed
+        cls.lines_cleared_sum += lines_cleared
+
+        print("EPISODE = "                   + str(cls.episode_count)     + 
+            " | PIECES PLACED = "            + str(cls.pieces_placed_sum) + 
+            " | PIECES PLACED IN EPISODE = " + str(pieces_placed) +
+            " | LINES = "                    + str(cls.lines_cleared_sum) +
+            " | LINES CLEARED IN EPISODE = " + str(lines_cleared))
+        cls.reset_log_counter()
+
+    def check_should_console_log_episodes(self, pieces_placed, lines_cleared) :
+
+        if EpisodeLogger.log_counter >= self.log_counter_max :
+            self.console_log_episode(pieces_placed=pieces_placed, lines_cleared=lines_cleared)
+            return True
+        return False
+        
 
 def sigmoid_scale(x, k=5.0):
     return (2.0 / (1.0 + np.exp(-x / k))) - 1.0
@@ -158,6 +195,8 @@ games = Tetris_AGENT.MultiGame(32)
 model = DQN(input_dim=39, output_dim=41, target_max=set_target_in_actions)
 target_model = DQN(input_dim=39, output_dim=41, target_max=set_target_in_actions)
 
+logger = EpisodeLogger(100)
+
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
@@ -171,9 +210,6 @@ epsilon = 1
 
 action_count = 0
 sum_action_count = 0
-
-episode = 0
-episode_log_counter = 0
 
 pieces_placed = 0
 pieces_placed_counter = 0
@@ -192,16 +228,14 @@ while True:
 
     action_count = 0
 
-    games.render(str(episode))
+    games.render(0)
 
     model.check_if_should_set_new_target(policy_net=model, target_net=target_model)
-
-    if episode_log_counter >= 500 and episode > 0:
-        print("EPISODE = " + str(episode) + " | PIECES PLACED = " + str(pieces_placed) + " | LINES = " + str(games.getLinesCleared()))
-        episode_log_counter = 0
+    
+    if (logger.check_should_console_log_episodes(pieces_placed=pieces_placed, lines_cleared=games.getLinesCleared())) :
         sum_action_count = 0
         watch_counter += 1
-    
+
     # if watch_counter >= 10 :
     #     if graphics_init == False :
     #         graphics_init = True
@@ -209,7 +243,7 @@ while True:
     #     watch_counter = 0
 
     if graphics_init == True :
-        games.render(str(episode))
+        games.render(0)
 
     state_t = torch.FloatTensor(state)
     q_values = model(state_t)
@@ -253,8 +287,7 @@ while True:
 
         if step_i.lost == True :
             games.resetThis(i)
-            episode += 1
-            episode_log_counter += 1
+            logger.increase_log_counter()
             
     for i in range(len(state)) :
         
